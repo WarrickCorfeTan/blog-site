7a122714476cb88410f7bd9ff5b50de1fb2cb012
warrickct <warrickct@gmail.com>
2017-10-18 16:09:34 +1300

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
 	#anything starting with post/ then takes anything in the bracket and stores in var pk which is passed to the view (which has to be at least 1 digit) + '/' at the end
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
